f152298ffdf201627a6509f5f58f9987f3c84d39
vinay <vinay86bhamre@gmail.com>
2023-10-10 18:10:42 +0530

    installed  react and tailwindcss and cleaned up boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className=" text-center text-3xl mt-10 text-red-600">
+        Online Judge Project
+      </h1>
+    </>
+  );
+}
+
+export default App;
